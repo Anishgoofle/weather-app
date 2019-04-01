2b10fb2557eb3354014556a25cb5c04f8aafc909
Anishgoofle <anishojha82@gmail.com>
2019-04-02 01:21:05 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ const API_KEY = "40211af4fe9d576a92ad2ce150d0fb39";
 
 class App extends Component {
 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -14,6 +23,14 @@ class App extends Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    });
   }
 
   render() {
